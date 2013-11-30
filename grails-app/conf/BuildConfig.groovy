13e694f210e6307af51ab969a17c681c6db8740f
Goran Ehrsson <goran@technipelago.se>
2013-11-30 18:58:05 +0100

    Upgraded codenarc to 0.19 and code-coverage to 1.2.7

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -1,38 +1,67 @@
-grails.project.class.dir = "target/classes"
-grails.project.test.class.dir = "target/test-classes"
-grails.project.test.reports.dir = "target/test-reports"
+grails.project.work.dir = "target"
 
 grails.project.repos.default = "crm"
 
 grails.project.dependency.resolution = {
-    // inherit Grails' default dependencies
-    inherits("global") {
-        // uncomment to disable ehcache
-        // excludes 'ehcache'
-    }
+    inherits("global") {}
     log "warn"
+    legacyResolve false
     repositories {
         grailsHome()
         mavenRepo "http://labs.technipelago.se/repo/crm-releases-local/"
         grailsCentral()
+        mavenCentral()
     }
     dependencies {
+        test "org.spockframework:spock-grails-support:0.7-groovy-2.0"
     }
 
     plugins {
         build(":tomcat:$grailsVersion",
-                ":release:2.2.1") {
+                ":release:2.2.1",
+                ":rest-client-builder:1.0.3") {
             export = false
         }
-        runtime(":hibernate:$grailsVersion") {
-            export = false
-        }
+        runtime ":hibernate:$grailsVersion"
 
-        test(":spock:0.7") { export = false }
-        test(":codenarc:0.17") { export = false }
+        test(":spock:0.7") {
+            export = false
+            exclude "spock-grails-support"
+        }
+        test(":codenarc:0.19") { export = false }
+        test(":code-coverage:1.2.7") { export = false }
 
         compile(":platform-core:1.0.RC5") { excludes 'resources' }
 
         compile "grails.crm:crm-core:latest.integration"
     }
+}
+
+codenarc {
+    reports = {
+        CrmXmlReport('xml') {
+            outputFile = 'target/CodeNarcReport.xml'
+            title = 'GR8 CRM CodeNarc Report'
+        }
+        CrmHtmlReport('html') {
+            outputFile = 'target/CodeNarcReport.html'
+            title = 'GR8 CRM CodeNarc Report'
+
+        }
+    }
+    properties = {
+        GrailsPublicControllerMethod.enabled = false
+        CatchException.enabled = false
+        CatchThrowable.enabled = false
+        ThrowException.enabled = false
+        ThrowRuntimeException.enabled = false
+        GrailsStatelessService.enabled = false
+        GrailsStatelessService.ignoreFieldNames = "dataSource,scope,sessionFactory,transactional,*Service,messageSource,grailsApplication,applicationContext,expose"
+    }
+    processTestUnit = false
+    processTestIntegration = false
+}
+
+coverage {
+    exclusions = ['**/radar/**']
 }
